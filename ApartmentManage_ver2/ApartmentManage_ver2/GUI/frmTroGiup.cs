7fa55e76e9d204109c78f4cede7051860d53c2ac
= <=>
2020-07-15 21:53:44 +0700

    Happiness Home

---
+++ ApartmentManage_ver2/ApartmentManage_ver2/GUI/frmTroGiup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Net;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace ApartmentManage_ver2.GUI
+{
+    public partial class frmTroGiup : DevExpress.XtraEditors.XtraForm
+    {
+        public frmTroGiup()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTroGiup_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("0396355253", "Số điện thoại liên hệ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("phantuananhltt@gmail.com", "Gmail", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("0589273382", "Số điện thoại liên hệ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("nguyenduchuy19437@gmail.com", "Gmail", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("0783583361", "Số điện thoại liên hệ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("lediemquynh999@gmail.com", "Số điện thoại liên hệ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("0906413301", "Gmail", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("c25quynhnhung173@gmail.com", "Gmail", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
